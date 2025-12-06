6490ec82e0bcdc44ded7fc9027de681d9801ffcf
Luis <108845020+xiLuiis@users.noreply.github.com>
2025-12-05 22:17:26 -0600

    Refactor WiFi credentials handling: move sensitive data to secrets.h and add .gitignore rule

--- firmware/impactalert_esp32/impactalert_esp32.ino
+++ firmware/impactalert_esp32/impactalert_esp32.ino
@@ -1,12 +1,18 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include "secrets.h"
 
-const char* ssid = "...";
-const char* password = "a0919c310b";
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
 bool emergencyActive = false;
+bool powerOn = false;
 
 WebServer server(80);
 
+// ----------------------------------
+// CANCELAR EMERGENCIA
+// ----------------------------------
 void handleCancel() {
   Serial.println("Comando recibido: CANCELAR EMERGENCIA");
   emergencyActive = false;
@@ -17,57 +23,71 @@ void handleCancel() {
   server.send(200, "application/json", json);
 }
 
-void handleStatus() {
-  String json = "{";
-  json += "\"connected\": true, ";
-  json += "\"emergency\": " + String(emergencyActive ? "true" : "false");
-  json += "}";
-
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "application/json", json);
-}
 // ----------------------------------
-
+// STATUS (ÚNICA VERSIÓN CORRECTA)
+// ----------------------------------
 void handleStatus() {
   String json = "{";
   json += "\"connected\": true, ";
   json += "\"emergency\": " + String(emergencyActive ? "true" : "false") + ",";
   json += "\"power\": " + String(powerOn ? "true" : "false");
   json += "}";
-  
+
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "application/json", json);
 }
 
+// ----------------------------------
+// ENCENDER
+// ----------------------------------
 void handleEncender() {
+  Serial.println("Comando recibido: ENCENDER");
   powerOn = true;
+
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "application/json", "{\"status\":\"OK\",\"action\":\"encender\"}");
 }
 
+// ----------------------------------
+// APAGAR
+// ----------------------------------
 void handleApagar() {
+  Serial.println("Comando recibido: APAGAR");
   powerOn = false;
+
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "application/json", "{\"status\":\"OK\",\"action\":\"apagar\"}");
 }
 
+// ----------------------------------
+// LEER SENSOR (simulado)
+// ----------------------------------
 void handleLeer() {
   float temp = 20 + random(0, 100) / 10.0;
+
   String json = "{\"temp\": " + String(temp, 2) + "}";
   Serial.println("Enviando datos simulados: " + json);
+
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "application/json", json);
 }
 
+// ----------------------------------
+// TRIGGER MANUAL DE EMERGENCIA
+// ----------------------------------
 void handleTriggerEmergency() {
   Serial.println("Emergencia ACTIVADA manualmente");
   emergencyActive = true;
 
   String json = "{ \"status\": \"emergency_on\", \"emergency\": true }";
+
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "application/json", json);
 }
 
+// ----------------------------------
+// SETUP
+// ----------------------------------
 void setup() {
   Serial.begin(115200);
 
---
+++ firmware/impactalert_esp32/secrets.h
@@ -0,0 +1,7 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "..."
+#define WIFI_PASSWORD "a0919c310b"
+
+#endif
